06bc9ea4d84d90bf38e5a221f785330ecb3660d8
shiva kumar gattu <gshiva0018@gmail.com>
2025-01-26 22:42:51 +0530

    flated array

---
+++ dublicate.js
@@ -0,0 +1,4 @@
+let a=[1,2,3,4,5,44,5,5,6,6,6,7,8]
+
+let removeDub=[...new Set(a)]
+console.log(removeDub)
---
+++ flattedarray.js
@@ -0,0 +1,4 @@
+let flatred=[1,[2,3,[4,5,6,[7,8,9,[10]]]]];
+
+let flatedArry=flatred.flat(Infinity)
+console.log(flatedArry)
