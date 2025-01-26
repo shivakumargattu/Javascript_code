bb0b28c3c2da32542fb75e16b3d2de6d4ae26be6
shiva kumar gattu <gshiva0018@gmail.com>
2025-01-26 21:08:09 +0530

    reverser

--- reversestrinf.js
+++ reversestrinf.js
@@ -4,3 +4,15 @@ let Str="haii how are you"
 
 let reveserStr= Str.split("").reverse().join("")
 console.log(reveserStr)
+
+
+// second method:
+
+let rev=""
+
+for (let i=Str.length; i>=0; i--){
+
+ rev+=Str[i]
+
+}
+console.log(rev)
